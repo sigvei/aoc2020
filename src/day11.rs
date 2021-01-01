ccb6dcc68c8d8618c2582e8c423f5f00ff2464be
Sigve Indregard <sigve@indregard.no>
2021-01-01 14:29:08 +0100

    Day 11 part 2
    Make the seat detection work with the nearest in line, not just the neighbors

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,3 @@
-use num::clamp;
 use std::collections::HashMap;
 
 #[derive(Clone)]
@@ -15,18 +14,18 @@ impl Seat {
         }
     }
 
-    fn ch(&self) -> char {
-        if self.occupied {
-            '#'
-        } else {
-            'L'
-        }
-    }
+    // fn ch(&self) -> char {
+    //     if self.occupied {
+    //         '#'
+    //     } else {
+    //         'L'
+    //     }
+    // }
 
-    fn prepare_state(&mut self, occupied_neighbours: usize) {
+    fn prepare_state(&mut self, occupied_neighbours: usize, death_rule: usize) {
         if !self.occupied && occupied_neighbours == 0 {
             self.next_state = true;
-        } else if self.occupied && occupied_neighbours >= 4 {
+        } else if self.occupied && occupied_neighbours >= death_rule {
             self.next_state = false;
         } else {
             self.next_state = self.occupied;
@@ -47,6 +46,17 @@ struct WaitingRoom {
 }
 
 impl WaitingRoom {
+    const DIRECTIONS: [(i32, i32); 8] = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (0, -1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+    ];
+
     fn new() -> WaitingRoom {
         WaitingRoom {
             rows: 0,
@@ -71,56 +81,66 @@ impl WaitingRoom {
         self.seats.values().filter(|seat| seat.occupied).count()
     }
 
-    fn neighbors_of(&self, row: &usize, col: &usize) -> Vec<&Seat> {
+    fn out_of_bounds(&self, row: i32, col: i32) -> bool {
+        // i32 means this will silently fail if rows or cols > 2**31
+        row >= self.rows as i32 || row < 0 || col >= self.cols as i32 || col < 0
+    }
+
+    fn nearest_in_lines_from(&self, row: i32, col: i32, max_dist: Option<i32>) -> Vec<&Seat> {
         let mut nbs = Vec::new();
-        let rows = &self.rows;
-        let cols = &self.cols;
-        let rmin = clamp(*row, 1, *rows) - 1;
-        let rmax = clamp(*row, 0, *rows - 2) + 1;
-        let cmin = clamp(*col, 1, *cols) - 1;
-        let cmax = clamp(*col, 0, *cols - 2) + 1;
-        for r in rmin..=rmax {
-            for c in cmin..=cmax {
-                if !(r == *row && c == *col) {
-                    if let Some(seat) = self.seats.get(&(r, c)) {
-                        nbs.push(seat);
+        for direction in &WaitingRoom::DIRECTIONS {
+            let mut i: i32 = 0;
+            loop {
+                i += 1;
+                if let Some(max) = max_dist {
+                    if i > max {
+                        break;
                     }
+                }
+
+                let pos = (row + direction.0 * i, col + direction.1 * i);
+                if self.out_of_bounds(pos.0, pos.1) {
+                    break;
+                }
+                if let Some(seat) = self.seats.get(&(pos.0 as usize, pos.1 as usize)) {
+                    nbs.push(seat);
+                    break;
                 }
             }
         }
         nbs
     }
 
-    fn tick(&mut self) {
+    fn tick(&mut self, distance_limit: Option<i32>, death_rule: usize) {
         let prev_state = self.clone();
         for ((row, col), seat) in self.seats.iter_mut() {
             let n = prev_state
-                .neighbors_of(row, col)
+                .nearest_in_lines_from(*row as i32, *col as i32, distance_limit)
                 .iter()
                 .filter(|nb| nb.occupied)
                 .count();
-            seat.prepare_state(n);
+            seat.prepare_state(n, death_rule);
             seat.commit_state();
         }
         self.ticks += 1;
     }
 
-    fn print(&self) {
-        println!("Grid: {}x{}, tick {}", self.cols, self.rows, self.ticks);
-        for row in 0..self.rows {
-            for col in 0..self.cols {
-                let ch: char = match self.seats.get(&(row, col)) {
-                    Some(seat) => seat.ch(),
-                    None => '.',
-                };
-                print!("{}", ch);
-            }
-            println!();
-        }
-    }
+    // fn print(&self) {
+    //     println!("Grid: {}x{}, tick {}", self.cols, self.rows, self.ticks);
+    //     for row in 0..self.rows {
+    //         for col in 0..self.cols {
+    //             let ch: char = match self.seats.get(&(row, col)) {
+    //                 Some(seat) => seat.ch(),
+    //                 None => '.',
+    //             };
+    //             print!("{}", ch);
+    //         }
+    //         println!();
+    //     }
+    // }
 }
 
-pub fn calculate(input: &str) {
+fn run(input: &str, distance_limit: Option<i32>, death_rule: usize) {
     let mut room = WaitingRoom::new();
 
     for (row, line) in input.split('\n').enumerate() {
@@ -137,7 +157,7 @@ pub fn calculate(input: &str) {
     let mut this_num: usize = 0;
     loop {
         prev_num = this_num;
-        room.tick();
+        room.tick(distance_limit, death_rule);
         this_num = room.taken();
         if prev_num == this_num {
             break;
@@ -147,4 +167,11 @@ pub fn calculate(input: &str) {
         "Stabilised at {} taken seats after {} ticks",
         this_num, room.ticks
     );
+}
+
+pub fn calculate(input: &str) {
+    println!("Calculating for max_distance=1 and death rule=4:");
+    run(input, Some(1), 4);
+    println!("Calculating for max_distance=Inf and death rule=5:");
+    run(input, None, 5);
 }
