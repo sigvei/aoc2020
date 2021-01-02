dee66f538d49a17dda0f4779c10b75a0f44589ea
Sigve Indregard <sigve@indregard.no>
2021-01-02 19:58:50 +0100

    Day15, now with progress bar

--- src/day15.rs
+++ src/day15.rs
@@ -1,13 +1,16 @@
+use progress_bar::color::{Color, Style};
+use progress_bar::progress_bar::ProgressBar;
 use std::collections::HashMap;
 
+static MAX: u32 = 30000000;
+
 pub fn calculate(input: &str) {
-    let mut last_occurred: HashMap<u64, u64> = HashMap::new();
-    let mut turn: u64 = 1;
-    let mut next: u64 = 0;
+    let mut last_occurred: HashMap<u32, u32> = HashMap::new();
+    let mut turn: u32 = 1;
+    let mut next: u32 = 0;
 
     for line in input.split('\n') {
-        if let Ok(num) = u64::from_str_radix(line, 10) {
-            println!("{}: {}", turn, num);
+        if let Ok(num) = u32::from_str_radix(line, 10) {
             last_occurred.insert(num, turn);
             next = num;
             turn += 1;
@@ -17,7 +20,10 @@ pub fn calculate(input: &str) {
     last_occurred.remove(&next);
     turn -= 1;
 
-    while turn <= 30000000 {
+    let mut bar = ProgressBar::new(MAX as usize);
+    bar.set_action("Calculating...", Color::Green, Style::Normal);
+
+    while turn <= MAX {
         let this = next;
         if let Some(occurrence) = last_occurred.get(&next) {
             next = turn - occurrence;
@@ -26,8 +32,17 @@ pub fn calculate(input: &str) {
         }
         last_occurred.insert(this, turn);
         turn += 1;
-        if turn == 2020 || turn == 30000000 || turn % 1000000 == 0 {
-            println!("{}: {}", turn, next);
+        if turn % 100000 == 0 {
+            bar.set_progression(turn as usize);
+        }
+
+        if turn == 2020 || turn == MAX {
+            bar.print_info(
+                "Success",
+                &format!("{}: {}", turn, next),
+                Color::Green,
+                Style::Normal,
+            );
         }
     }
 }
